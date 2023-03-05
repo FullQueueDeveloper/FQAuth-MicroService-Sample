687145d869bfaf765baab5420a654be8364bd8c6
Full Queue Developer <FullQueueDeveloper@FullQueueDeveloper.com>
2023-03-04 23:51:55 -0500

    use extracted middleware

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.1.3"),
     .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.3.0"),
 
-    .package(url: "https://github.com/FullQueueDeveloper/FQAuth", branch: "initial"),
+    .package(url: "https://github.com/FullQueueDeveloper/FQAuthMiddleware", from: "0.1.2"),
     .package(url: "https://github.com/FullQueueDeveloper/Sh", from: "1.0.0"),
 
   ],
@@ -30,7 +30,7 @@ let package = Package(
       .product(name: "PostgresKit", package: "postgres-kit"),
       .product(name: "Fluent", package: "fluent"),
       .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
-      .product(name: "FQAuthMiddleware", package: "FQAuth"),
+      .product(name: "FQAuthMiddleware", package: "FQAuthMiddleware"),
       "Sh",
     ],
     swiftSettings: [
